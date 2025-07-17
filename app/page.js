4195c192a52e8275919b34ef335055868fc48fa4
RadhaKrishna <radhakrishna@radhakrshnasair.in.ibm.com>
2025-07-17 14:45:36 +0530

    added pipeline in azure

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '100px' }}>
       <h1>Hello World from Next.js 🚀</h1>
+      <h1>Testing Pipeline</h1>
     </div>
   );
 }
